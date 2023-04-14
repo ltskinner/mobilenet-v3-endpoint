35dcbd273ce0b655dcb4c702d12ce4f1e894b387
ltskinner <33165286+ltskinner@users.noreply.github.com>
2023-04-13 21:48:11 -0500

    cleaned up and changed exe host and port

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,6 @@
 
-import os
-
-print(os.getcwd())
-print(os.listdir())
-
-
 from webapp.routes import application
 
 
 if __name__ == "__main__":
-    application.run(host="127.0.0.1", port=8080, debug=True)
+    application.run(host="0.0.0.0", port=8080, debug=True)
